406f95c6ef1e809cccc284b98a0cefc98ce06f1b
uh-zz <uhzz.contact@gmail.com>
2021-01-16 15:23:42 +0900

    add about contact controller

--- rails/tutorial/myapp/config/routes.rb
+++ rails/tutorial/myapp/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  resources :microposts
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'

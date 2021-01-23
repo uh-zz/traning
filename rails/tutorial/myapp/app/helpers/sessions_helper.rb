08695d3f81694daaa5a57e33336bebdde1d72848
uh-zz <uhzz.contact@gmail.com>
2021-01-23 18:01:32 +0900

    add remember_me checkbox

--- rails/tutorial/myapp/app/helpers/sessions_helper.rb
+++ rails/tutorial/myapp/app/helpers/sessions_helper.rb
@@ -16,13 +16,13 @@ module SessionsHelper
     def current_user
         if (user_id = session[:user_id])
             @current_user ||= User.find_by(id: user_id)
-          elsif (user_id = cookies.signed[:user_id])
+        elsif (user_id = cookies.signed[:user_id])
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
-              log_in user
-              @current_user = user
+                log_in user
+                @current_user = user
             end
-          end
+        end
     end
 
     # ユーザーがログインしていればtrue、その他ならfalseを返す

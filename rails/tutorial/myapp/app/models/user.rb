8a467df850a56004ed45a32ac3fa89799276b752
uh-zz <uhzz.contact@gmail.com>
2021-02-05 18:31:49 +0900

    add follow test

--- rails/tutorial/myapp/app/models/user.rb
+++ rails/tutorial/myapp/app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
     has_many :active_relationships, class_name:  "Relationship",
         foreign_key: "follower_id",
         dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
 
     validates :name, presence: true, length:{ maximum:50 }
 
@@ -84,6 +85,17 @@ class User < ApplicationRecord
         Micropost.where("user_id = ?", id)
     end
 
+    def follow(other_user)
+        following << other_user
+    end
+
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
+    end
     
     private
 

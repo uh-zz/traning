1cecad50b8fb3b390a2db5fdda310a69383536c9
uh-zz <uhzz.contact@gmail.com>
2021-02-05 19:12:33 +0900

    add button to follow in home

--- rails/tutorial/myapp/app/models/user.rb
+++ rails/tutorial/myapp/app/models/user.rb
@@ -5,10 +5,18 @@ class User < ApplicationRecord
 
     # has_many -> UserとMicropostは 1対多
     has_many :microposts, dependent: :destroy
+
+    # 能動的関係 -> フォローする
     has_many :active_relationships, class_name:  "Relationship",
-        foreign_key: "follower_id",
-        dependent:   :destroy
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
     has_many :following, through: :active_relationships, source: :followed
+
+    # 受動的関係 -> フォローされる
+    has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
 
     validates :name, presence: true, length:{ maximum:50 }
 

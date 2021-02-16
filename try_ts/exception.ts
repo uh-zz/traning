4f4b1ac417e88ae213fb0b52624fe0caf6f21c01
uh-zz <uhzz.contact@gmail.com>
2021-02-16 17:37:42 +0900

    add how to use inheritance

--- try_ts/exception.ts
+++ try_ts/exception.ts
@@ -18,9 +18,55 @@
         console.log("fetch end")
     }
     
-    const e = new Error("hoge")
-    console.log(`e.name: ${e.name}`)
-    console.log(`e.message: ${e.message}`)
-    console.log(`e.stack: ${e.stack}`)
+    // const e = new Error("hoge")
+    // console.log(`e.name: ${e.name}`)
+    // console.log(`e.message: ${e.message}`)
+    // console.log(`e.stack: ${e.stack}`)
     
 })();
+
+// リカバリー処理分岐のためにユーザ定義の例外クラスを作成
+//
+//
+// 立派な継承ツリーを作る必要はない
+//
+//
+//
+// まずは共通クラスを作成
+class BaseError extends Error {
+    constructor(e?: string) {
+        super(e);
+        this.name = new.target.name;
+    }
+}
+
+// BaseErrorを継承した独自クラスを作成
+class NetWorkAccessError extends BaseError {
+    constructor(public statusCode: number, e?: string) {
+        super(e)
+    }
+}
+
+// 追加属性がなければコンストラクタも定義不要
+class NoNetWorkError extends BaseError {}
+
+
+(async() => {
+    try {
+        await getUser();
+    } catch(e) {
+        if (e instanceof NoNetWorkError) {
+            console.log(`no network`)
+        } else if (e instanceof NetWorkAccessError) {
+            if (e.statusCode < 500) {
+                console.log(`program has bug`)
+            } else {
+                console.log(`server error`)
+            }
+        }
+    }
+})();
+
+async function getUser() {
+    throw new NetWorkAccessError(401, "hoge");
+}

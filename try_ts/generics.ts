5b54843300bcb407a0119d3eb69df45b5a65f8bb
uh-zz <uhzz.contact@gmail.com>
2021-02-18 15:11:14 +0900

    how to use keyof

--- try_ts/generics.ts
+++ try_ts/generics.ts
@@ -23,3 +23,67 @@ console.log(multiply<number>(-1, 5));
 
 // 型推論も可能！！！
 console.log(multiply("mazika", 10));
+
+// 型パラメータに制約をつけたい場合
+//
+//
+// 引数のジェネリクスの型を満たすようなインターフェースを用意
+interface Person4 {
+    getBirthDay(): Date;
+}
+
+function isTodayBirthday<T extends Person4>(person: T): boolean {
+    const today = new Date();
+
+    const birthDay = person.getBirthDay();
+
+    return today.getMonth() === birthDay.getMonth() &&
+        today.getDate() === birthDay.getDate();
+}
+
+// keyofの使い方
+//
+// TypeScript 2.1で追加された
+//
+// オブジェクトと key を受け取り、値を返す関数
+// 
+// このようなインターフェースを用意
+// interface ISomeObject {
+//     firstKey: string;
+//     seocondKey: string;
+// }
+//
+// こうすると暗黙的にany型が返ってくるのでコンパイラでエラーになる
+// function getProperty(obj: ISomeObject, key: string) {
+//     return obj[key];
+// }
+//
+//
+// 解決法1
+// インターフェースに keyの型と返り値の型を定義する
+// interface ISomeObject {
+//     firstKey: string;
+//     seocondKey: string;
+//     [key: string]: string; // このような定義を追加
+// }
+//
+//
+// 解決法2
+// keyof を使う
+interface ISomeObject {
+    firstKey: string;
+    seocondKey: string;
+    2: string;
+}
+
+// keyof type/interfaceという構文にすることで、
+// オブジェクトのkey, value情報を取得してくれる
+// 
+function getProperty(obj: ISomeObject, key: keyof ISomeObject) {
+    return obj[key];
+}
+
+// ジェネリクスで書くならこんな感じ
+function getPropertyGen<S, T extends keyof S>(obj: S, key: T) {
+        return obj[key];
+}

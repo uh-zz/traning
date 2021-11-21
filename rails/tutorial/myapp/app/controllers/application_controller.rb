08d23ad24a90108db44a4db534a50b663d7921de
uh-zz <uhzz.contact@gmail.com>
2021-11-21 22:11:00 +0900

    :+1: formatterとlinterをかけてみた

--- rails/tutorial/myapp/app/controllers/application_controller.rb
+++ rails/tutorial/myapp/app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
-   
-    protect_from_forgery with: :exception
-    include SessionsHelper
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
-    private
+  private
 
-    # ユーザーのログインを確認する
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+  # ユーザーのログインを確認する
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
+  end
 end
--- rails/tutorial/myapp/app/controllers/password_resets_controller.rb
+++ rails/tutorial/myapp/app/controllers/password_resets_controller.rb
@@ -1,26 +1,23 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
-  before_action :check_expiration, only: [:edit, :update]
+  before_action :user,   only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
 
-  def new
-  end
+  def new; end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
+      flash[:info] = 'Email sent with password reset instructions'
       redirect_to root_url
     else
-      flash.now[:danger] = "Email address not found"
+      flash.now[:danger] = 'Email address not found'
       render 'new'
     end
-
   end
 
   def update
@@ -29,7 +26,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update(user_params)
       log_in @user
-      flash[:success] = "Password has been reset"
+      flash[:success] = 'Password has been reset'
       redirect_to @user
     else
       render 'edit'
@@ -42,24 +39,23 @@ class PasswordResetsController < ApplicationController
     params.require(:user).permit(:password, :password_confirmation)
   end
 
-  def get_user
+  def user
     @user = User.find_by(email: params[:email])
   end
 
   def valid_user
     @user = User.find_by(email: params[:email])
-    unless( @user && @user.activated? &&
-      @user.authenticated?(:reset, params[:id]) )
+    unless @user && @user&.activated? &&
+           @user&.authenticated?(:reset, params[:id])
 
       redirect_to root_url
     end
   end
 
-
   def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired"
-      redirect_to new_password_reset_url
-    end
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = 'Password reset has expired'
+    redirect_to new_password_reset_url
   end
 end

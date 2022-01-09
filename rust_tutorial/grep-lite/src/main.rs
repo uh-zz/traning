a82ac29257af73ecfb389a71d1824b96acf62022
uh-zz <uhzz.contact@gmail.com>
2022-01-09 15:30:51 +0900

    :recycle: enumerateでインデックスをつくる

--- rust_tutorial/grep-lite/src/main.rs
+++ rust_tutorial/grep-lite/src/main.rs
@@ -6,12 +6,11 @@ dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
 
-    let mut line_num: usize = 1;
-
-    for line in quote.lines() {
+    // enumerate()は、イテレータIを受け取り、イテレータ(N, I)を返す。Nは0から始まってインクリメントされる
+    for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
+            let line_num = i + 1;
             println!("{}: {}", line_num, line);
         }
-        line_num += 1;
     }
 }

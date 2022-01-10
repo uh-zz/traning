224be33e81170d634087c7f5800a6dfdfb6e7152
uh-zz <uhzz.contact@gmail.com>
2022-01-10 20:48:22 +0900

    :recycle: ファイルから検索できるようにリファクタリング

--- rust_tutorial/grep-lite3/src/main.rs
+++ rust_tutorial/grep-lite3/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let args = App::new("grep-lite3")
@@ -11,20 +14,25 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?
-";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }

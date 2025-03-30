9f9955b336c33bbd27658e59e6fb2bcbe5d0c234
uh-zz <uhzz.contact@gmail.com>
2025-03-30 18:57:19 +0900

    add: haskell: larger divisible

---
+++ haskell/larger_divisible.hs
@@ -0,0 +1,11 @@
+import Prelude hiding (filter)
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs)
+  | p x       = x : filter p xs
+  | otherwise = filter p xs
+
+largerDivisible :: Integer
+largerDivisible = head (filter p [1000000,999999..])
+    where p x = x `mod` 3829 == 0
+
